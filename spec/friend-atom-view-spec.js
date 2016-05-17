ecd3d4cdb67c6a97ea12b28f1f22d8a0b02d286f
oskar367 <oskar.jedvert@gmail.com>
2016-05-17 16:25:30 +0200

    initial commit

---
+++ spec/friend-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FriendAtomView from '../lib/friend-atom-view';
+
+describe('FriendAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
